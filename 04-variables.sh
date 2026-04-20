cb757c49b47693aa8e87b9eaaa6d60abd582db3c
pushpavathi-wq <valasapushpa13@gmail.com>
2026-04-20 12:36:39 +0530

    file 4

--- 04-variables.sh
+++ 04-variables.sh
@@ -4,7 +4,7 @@ person1=$1
 person2=$2
 
 
-echo "$person1:: Hi$person2"
-echo "$person2:: hello$person1"
+echo "$person1:: Hi $person2"
+echo "$person2:: hello $person1"
 echo "$person1:: go"
 echo "$person2:: i will go"
