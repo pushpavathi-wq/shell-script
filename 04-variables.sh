6207e2ad64a5f3e01d3b99f7d65ccec6e0d02a24
pushpavathi-wq <valasapushpa13@gmail.com>
2026-04-21 07:13:13 +0530

    commit changes

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-person1=$1     # variables declared
-person2=$2
-
-
-echo "$person1:: Hi $person2"
-echo "$person2:: hello $person1"
-echo "$person1:: go"
-echo "$person2:: i will go"
+echo "family members are: $@"
+echo "total numbers: $#"
