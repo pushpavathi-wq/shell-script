eccbd1a2b88d3d9fcc31ac40ffab0ce3446474f0
pushpavathi-wq <valasapushpa13@gmail.com>
2026-04-22 18:27:33 +0530

    commit service

---
+++ 12-service-status.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SERVICE =nginx
+
+if systemctl is-active --quiet $SERVICE  ### quiet is used to display nothing if service is active
+then
+echo "$SERVICE is active"
+else 
+echo "$SERVICE is not active"
+fi 
