044eb91feb07df85e0f9d6d0ec0d8925b2b73f1f
pushpavathi-wq <valasapushpa13@gmail.com>
2026-04-22 16:43:13 +0530

    commit

--- 11-timing.sh
+++ 11-timing.sh
@@ -3,4 +3,13 @@
 # password entering script
 
 
-read -s -p "enter password in 10 seconds: "
+read -s -p "enter password in 10 seconds: "password
+
+if [ -z "$password" ]
+then
+echo "no password enterd"
+else 
+echo "password entered:$password"
+fi 
+
+
