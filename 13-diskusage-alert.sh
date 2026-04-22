cfdeb6e8a09d89d16b35a9e04410c3e5a4d68eae
pushpavathi-wq <valasapushpa13@gmail.com>
2026-04-22 19:10:12 +0530

    commit service

---
+++ 13-diskusage-alert.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+THRESHOLD=90
+
+USAGE=$(df -h --output=pcent / | tail -1 | tr -d '%') # tr -translate
+
+if [ $USAGE -ge $THRESHOLD ]
+then
+echo "usage is high/disk almost full"
+fi 
